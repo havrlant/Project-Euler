038de671c8dbf3780d82018e7b3108506fd00128
Lukáš Havrlant <lukas.havrlant@gmail.com>
2013-01-22 13:18:20 +0100

    - type conversion

--- problem092.py
+++ problem092.py
@@ -5,10 +5,9 @@ from time import time
 t = time()
 
 factorials = [factorial(x) for x in xrange(10)]
-powers = {str(x):x**2 for x in xrange(10)}
 
 def digitPowers(n):
-    return sum(powers[x] for x in str(n))
+    return sum(x*x for x in n)
 
 def combinationCount(k, n):
     return factorials[n] / (factorials[n-k]*factorials[k])
@@ -23,7 +22,7 @@ for i in xrange(1, 7*(9**2) + 1):
         if i == 89:
             cachedDigitPowers[orig] = True
             break
-        i = digitPowers(i)
+        i = digitPowers(map(int, str(i)))
 
 # Count all possible permutations of some tuple
 # eg. [aaabbd] has 420 permutations: aaabbd, aaabdb, aaadbb, baabda, ...
@@ -41,21 +40,17 @@ def permutationsCount(counter):
 def getCombinations():
     toint = lambda x: reduce(lambda rst, d: rst * 10 + d, comb) # converts (1,2,3) to 123
     for c in xrange(1, 8):
-        for comb in combinations_with_replacement(range(1, 10), c):
-            i = toint(comb)  
-            if cachedDigitPowers[digitPowers(i)]:
-                yield i
+        for comb in combinations_with_replacement(range(1, 10), c): 
+            if cachedDigitPowers[digitPowers(comb)]:
+                yield comb
 
 def getCounter(n):
     d = {}
-    while n > 0:
-        mod = n % 10
-        c = d.get(mod, False)
-        if c:
-            d[mod] = c + 1
-        else:
-            d[mod] = 1
-        n /= 10
+    for x in n:
+        res = d.get(x, False)
+        if not res:
+            res = 0
+        d[x] = res + 1
     return d
 
 # Count all permutations: eg. if 1255 is in stucksIn89, then
