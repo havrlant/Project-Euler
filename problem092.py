3948ae193e110cd0ad23016aa49023b1ba7bf3f4
Lukáš Havrlant <lukas.havrlant@gmail.com>
2013-01-22 13:34:30 +0100

    refactoring

--- problem092.py
+++ problem092.py
@@ -6,23 +6,44 @@ t = time()
 
 factorials = [factorial(x) for x in xrange(10)]
 
+# 175 -> 1^2+7^2+5^2
 def digitPowers(n):
     return sum(x*x for x in n)
 
 def combinationCount(k, n):
     return factorials[n] / (factorials[n-k]*factorials[k])
 
-cachedDigitPowers = [False for x in xrange(1, 7*(9**2) + 2)]
+# 175 -> [5, 7, 1]
+def getDigits(n):
+    digits = []
+    while n > 0:
+        residue = n % 10
+        n /= 10
+        digits.append(residue)
+    return digits
+
+# [7,8,6,7,7,5] -> {8: 1, 5: 1, 6: 1, 7: 3}
+def getCounter(n):
+    d = {}
+    for x in n:
+        res = d.get(x, False)
+        if not res:
+            res = 0
+        d[x] = res + 1
+    return d
 
 # Determine the end of the first 567 numbers
-for i in xrange(1, 7*(9**2) + 1):
-    orig = i
-    while True:
-        if i == 1: break
-        if i == 89:
-            cachedDigitPowers[orig] = True
-            break
-        i = digitPowers(map(int, str(i)))
+def getFirst567DigitNumbers():
+    cachedDigitPowers = [False for x in xrange(1, 7*(9**2) + 2)]
+    for i in xrange(1, 7*(9**2) + 1):
+        orig = i
+        while True:
+            if i == 1: break
+            if i == 89:
+                cachedDigitPowers[orig] = True
+                break
+            i = digitPowers(getDigits(i))
+    return cachedDigitPowers
 
 # Count all possible permutations of some tuple
 # eg. [aaabbd] has 420 permutations: aaabbd, aaabdb, aaadbb, baabda, ...
@@ -38,25 +59,16 @@ def permutationsCount(counter):
 # Computes all _combinations_ of 123456789 of length 1..7,
 # which stucks in 89
 def getCombinations():
-    toint = lambda x: reduce(lambda rst, d: rst * 10 + d, comb) # converts (1,2,3) to 123
+    cachedDigitPowers = getFirst567DigitNumbers()
     for c in xrange(1, 8):
         for comb in combinations_with_replacement(range(1, 10), c): 
             if cachedDigitPowers[digitPowers(comb)]:
                 yield comb
-
-def getCounter(n):
-    d = {}
-    for x in n:
-        res = d.get(x, False)
-        if not res:
-            res = 0
-        d[x] = res + 1
-    return d
 
 # Count all permutations: eg. if 1255 is in stucksIn89, then
 # 1552, 5152, 51520, 15052, ... stucks in 89 too.
 def getAllCombsCount():
     return (permutationsCount(getCounter(num).values()) for num in getCombinations())
 
-print sum(getAllCombsCount()) # Yay! 
+print sum(getAllCombsCount()) == 8581146 # Yay! 
 print time() - t
